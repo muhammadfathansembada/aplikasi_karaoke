b77b2ec74d7088434e26b6c37eac4b15e9da08d8
muhammadfathansembada <77955483+muhammadfathansembada@users.noreply.github.com>
2021-01-27 20:07:49 +0700

    Create Login.vb

---
+++ Login.vb
@@ -0,0 +1,16 @@
+Public Class Login
+    Private Sub btnSubmit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSubmit.Click
+        login_valid = oUser.Login(txtUsername.Text, txtPassword.Text)
+        If (login_valid = True) Then
+            MessageBox.Show(" Yes Login Berhasil, Selamat Datang " & txtUsername.Text)
+            MenuUtama.Show()
+            Me.Hide()
+        Else
+            MessageBox.Show("Login Not Valid")
+        End If
+    End Sub
+
+    Private Sub btnCancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCancel.Click
+        Me.Close()
+    End Sub
+End Class
